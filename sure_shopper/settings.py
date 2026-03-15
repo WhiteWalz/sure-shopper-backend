788dde15a8e6be2001d70a7e814163e5dbdd56e4
J. White <whijus602@gmail.com>
2026-03-15 00:27:01 -0400

    Adds apps and gis to settings.py

--- sure_shopper/settings.py
+++ sure_shopper/settings.py
@@ -40,6 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'lists',
+    'users',
+    'locations',
+    'products',
+    'stores',
+    'pricing',
+    'django.contrib.gis',
 ]
 
 MIDDLEWARE = [
